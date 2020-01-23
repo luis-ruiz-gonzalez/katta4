6f99a5b7d4e94f12fb247cef5c8781d4a7a2ca2d
Luis Ruiz <Luis Ruiz@192.168.1.39>
2020-01-23 00:46:39 +0000

    Segunda versión de katta4

--- src/katta4/main/Katta4.java
+++ src/katta4/main/Katta4.java
@@ -8,12 +8,33 @@ import katta4.view.MailHistogramBuilder;
 import katta4.view.MailListReader;
 
 public class Katta4 {
-
+    private String fileName;
+    private List<Mail> mailList;
+    private Histogram histogram;
+    private HistogramDisplay histogramDisplay;
+    
     public static void main(String[] args) {
-        String fileName = new String("email.txt");
-        List<Mail> mailList = MailListReader.read(fileName);                       
-        Histogram histogram = MailHistogramBuilder.build(mailList);                  
-        HistogramDisplay histogramDisplay = new HistogramDisplay(histogram);
+        Katta4 katta4 = new Katta4();
+        katta4.execute();
+    }
+
+    private void execute(){
+        input();
+        process();
+        output();
+    }
+    
+    private void input() {
+        fileName = new String("email.txt");
+        mailList = MailListReader.read(fileName);
+    }
+
+    private void process() {
+        histogram = MailHistogramBuilder.build(mailList);  
+    }
+
+    private void output() {
+        histogramDisplay = new HistogramDisplay(histogram);
         histogramDisplay.execute();
     }
     
